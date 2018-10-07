4bf7e46dc83cd392ce9cd17fb04740c79f025f06
Lois Choi <insil321@hotmail.com>
2018-10-07 21:39:08 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,11 +1,10 @@
 def help 
-  help = << HELP
+  help = 
 I accept the following commands:
 - help : displays this help message
 - list : displays a list of songs you can play
 - play : lets you choose a song to play
 - exit : exits this program
-HELP 
 
 puts help
 end
