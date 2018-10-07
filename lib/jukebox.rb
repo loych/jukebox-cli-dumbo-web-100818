b78688c7f1d51f7bbb1c423d8f6729e3523bacd0
Lois Choi <insil321@hotmail.com>
2018-10-07 21:49:28 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,14 +1,4 @@
- def help 
-  help = 
-I accept the following commands:
-- help : displays this help message
-- list : displays a list of songs you can play
-- play : lets you choose a song to play
-- exit : exits this program
-puts help
-end
-end
-end
+
 
 def list(songs)
   songs.each_with_index { |item, index|
